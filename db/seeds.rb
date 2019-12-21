c713bdaea5c2b43ab21554efb54b7d1e885bad59
André Alencar <andreluis88@gmail.com>
2019-12-21 11:30:14 -0300

    Create seeds for dog breeds and dogs

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+['Bulldog', 'Beagle', 'Boxer', 'Chow Chow', 'Dalmatian'].each do |breed|
+  DogBreed.where(name: breed).first_or_create!
+end
+
+breeds = DogBreed.all
+['Coockie', 'Lilica', 'Boris', 'Laica', 'Morgana'].each do |dog|
+  Dog.where(name: dog)
+    .first_or_create!(
+      dog_breed: breeds.sample,
+      age: (0..10).to_a.sample
+    )
+end
